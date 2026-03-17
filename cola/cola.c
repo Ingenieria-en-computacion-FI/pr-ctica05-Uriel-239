00f16f8bdaad7c04cc6de72d9b20c5d0a338decd
Uriel Salgado <uri.auss@gmail.com>
2026-03-16 20:37:41 -0600

    Cast Cola to Lista in cola functions

--- cola/cola.c
+++ cola/cola.c
@@ -1,37 +1,35 @@
+#include <stdlib.h>
+#include <stdbool.h>
 #include "cola.h"
+#include "../lista/lista.h"
 
 Cola* cola_crear()
 {
-    return lista_crear();
+    return (Cola*)lista_crear();
 }
 
 int cola_vacia(Cola* cola)
 {
-    return lista_vacia(cola);
+    return lista_vacia((Lista*)cola);
 }
 
 void cola_enqueue(Cola* cola, int dato)
 {
-    lista_insertar_tail(cola, dato);
+    lista_insertar_tail((Lista*)cola, dato);
 }
 
 int cola_dequeue(Cola* cola)
 {
-    if (cola_vacia(cola)) {
-        return -1; 
-   }
-    return lista_eliminar_head(cola);
+    return lista_eliminar_head((Lista*)cola);
 }
 
 int cola_frente(Cola* cola)
 {
-    if (cola_vacia(cola)) {
-        return -1;
-    }
-    return lista_obtener_head(cola);
+    if (cola_vacia(cola)) return -1;
+    return ((Lista*)cola)->head->dato;
 }
 
 void cola_destruir(Cola* cola)
 {
-    lista_destruir(cola);
+    lista_destruir((Lista*)cola);
 }
