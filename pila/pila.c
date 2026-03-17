5af98564aca026293c36ea24782223cf86671ba5
Uriel Salgado <uri.auss@gmail.com>
2026-03-16 18:15:34 -0600

    Implement stack functions using list operations

--- pila/pila.c
+++ pila/pila.c
@@ -1,49 +1,52 @@
+#include <stdlib.h>
 #include "pila.h"
+#include "lista.h"
 
 Pila* pila_crear()
 {
-    /*
-    TODO
-    usar crear lista
-    */
+    Pila* nuevaPila = (Pila*)malloc(sizeof(Pila));
+    if (nuevaPila != NULL) {
+        nuevaPila->lista = lista_crear();
+    }
+    return nuevaPila;
 }
 
 int pila_vacia(Pila* pila)
 {
-    /*
-    TODO
-    usar lista_vacia
-    */
+    if (pila == NULL || lista_vacia(pila->lista)) {
+        return 1;
+    }
+    return 0;
 }
 
 void pila_push(Pila* pila, int dato)
 {
-    /*
-    TODO
-    usar lista_insertar_tail
-    */
+    if (pila != NULL) {
+        lista_insertar_tail(pila->lista, dato);
+    }
 }
 
 int pila_pop(Pila* pila)
 {
-    /*
-    TODO
-    usar lista_eliminar_tail
-    */
-    return -1;
+    if (pila_vacia(pila)) {
+        return -1; 
+    }
+    return lista_eliminar_tail(pila->lista);
 }
 
 int pila_top(Pila* pila)
 {
-    /*
-    TODO
-    regresar dato del tail
-    */
-    return -1;
+    if (pila_vacia(pila)) {
+        return -1;
+    }
+    
+    return pila->lista->tail->dato;
 }
 
 void pila_destruir(Pila* pila)
 {
-    // TODO
-    // Usar lista destruir
+    if (pila != NULL) {
+        lista_destruir(pila->lista);
+        free(pila);
+    }
 }
