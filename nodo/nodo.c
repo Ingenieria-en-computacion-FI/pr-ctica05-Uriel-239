e68c6515ed61de69b6cf21477e54008e22831364
Uriel Salgado <uri.auss@gmail.com>
2026-03-16 18:12:39 -0600

    Refactor nodo_crear and nodo_destruir functions

--- nodo/nodo.c
+++ nodo/nodo.c
@@ -1,19 +1,17 @@
 #include <stdlib.h>
 #include "nodo.h"
 
-Nodo* nodo_crear(int dato)
-{
-    Nodo* PlusNodo= (Nodo*)malloc(sizeof(Nodo));
-    if(PlusNodo != NULL){
-        PlusNodo -> dato = dato;
-        PlusNodo -> sig = NULL;
+Nodo* nodo_crear(int dato) {
+    Nodo* PlusNodo = (Nodo*)malloc(sizeof(Nodo));
+    if(PlusNodo != NULL) {
+        PlusNodo->dato = dato;
+        PlusNodo->sig = NULL; 
     }
     return PlusNodo;
 } 
 
-void nodo_destruir(Nodo* nodo)
-{
-    if(nodo != NULL){
+void nodo_destruir(Nodo* nodo) {
+    if(nodo != NULL) {
         free(nodo);
     } 
 }
